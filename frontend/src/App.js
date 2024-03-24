e0087aa654c79593d1db05037b418ca00365bf35
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-03-24 19:58:17 +0100

    changed ip to 127.0.0.1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 axios.defaults.withCredentials = true;
 
 const client = axios.create({
-  baseURL: "http://152.10.2.2:8000"
+  baseURL: "http://127.0.0.1:8000"
 });
 
 function App() {
