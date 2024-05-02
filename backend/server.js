d672f56f1ea683dd7a01e0ab935387defd72a858
Shetteemah <shettimaali58@yahoo.com>
2024-05-02 20:14:06 +0200

    created server.js

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('New client connected:', socket.id);
+
+    socket.on('send-message', (data) => {
+        console.log('Message received:', data);
+        // Broadcast the message to all sockets except the sender
+        socket.broadcast.emit('new-message', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected:', socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
